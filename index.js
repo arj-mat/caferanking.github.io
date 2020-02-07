8f352ce0e39f172589655da2b582f51b781dbcfa
Matheus S <51185184+whatsupMate@users.noreply.github.com>
2020-02-07 20:40:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 fetch("https://caferanking.000webhostapp.com/get.php?&e=json&f=cafe")
 .then((resp) => resp.json())
 .then(function(data) {
-    let ranking = Object.entries(data).sort((a, b) => a[1] < b[1])
+    var ranking = Object.entries(data).sort((a, b) => a[1] < b[1])
     for (var i = 0; i < 100; i++) {
-        let tr = document.createElement("tr");
+        varr tr = document.createElement("tr");
 
-        let td = document.createElement("td");
+        var td = document.createElement("td");
         td.id = "position";
         td.id = "user-rank";
         td.innerHTML = "#" +(i + 1) 
 
-        let tdNick = document.createElement("td");
+        var tdNick = document.createElement("td");
         tdNick.id = "user-info";
         tdNick.innerHTML = `<span id="user-name">${ranking[i][0]}</span>`
 
-        let tdMessages = document.createElement("td");
+        var tdMessages = document.createElement("td");
         tdMessages.innerHTML = ranking[i][1]
 
         tr.appendChild(td)
